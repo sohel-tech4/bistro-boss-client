ea156998fa95e7e642ea210de42bdd472e6f1113
soheltech <soheltech4@gmail.com>
2025-07-05 02:13:52 +0600

    add banner

--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
               {NavOptions}
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="btn btn-ghost text-xl">BISTRO BOSS</a>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">{NavOptions}</ul>
